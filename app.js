8f9c1b578360dc649fdacc4f0c008fdb4d10675a
Nursultan9722 <m.nursultan_27@mail.ru>
2022-02-27 21:32:34 +0600

    add indexRouter, userRouter

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+require('./models/index');
 var app = express();
 
 app.use(logger('dev'));
@@ -15,6 +14,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const auth = require('./auth')
+const products = require('./products');
+const users = require('./users');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.use('/auth', auth);
+router.use('/users', users);
+router.use('/products', products);
 
 module.exports = router;
